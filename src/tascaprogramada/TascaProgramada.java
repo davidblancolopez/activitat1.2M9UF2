287e79a620b71bcacd596711ecc0a9fcbd322a7a
davidblancolopez <daviddbl94@gmail.com>
2016-10-14 17:23:28 +0200

    Ejercicio 1.2 de M9 UF2

---
+++ src/tascaprogramada/TascaProgramada.java
@@ -0,0 +1,24 @@
+
+package tascaprogramada;
+
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+
+
+public class TascaProgramada {
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        ScheduledExecutorService executor = (ScheduledExecutorService) Executors.newScheduledThreadPool(2);
+        imprimeixExecucioTasca  iet = new imprimeixExecucioTasca ();
+        
+        executor.scheduleWithFixedDelay(iet, 2, 3, TimeUnit.SECONDS);
+        
+        iet.terminaServidor();
+    }
+    
+    
+}
---
+++ src/tascaprogramada/imprimeixExecucioTasca.java
@@ -0,0 +1,38 @@
+package tascaprogramada;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+
+public class imprimeixExecucioTasca {
+
+    Calendar calendario = new GregorianCalendar();
+    
+    private final ScheduledExecutorService executor;
+    
+    public imprimeixExecucioTasca() {
+        executor = (ScheduledExecutorService) Executors.newScheduledThreadPool(2);
+    }
+    
+    
+    public void run() {
+        DateFormat formatoHora = new SimpleDateFormat("HH:mm:ss");
+        System.out.println(Thread.currentThread().getName() + "      "+ " Hora: " + formatoHora.format(new Date()));
+        
+    }
+    
+    
+    public void terminaServidor() {
+        executor.shutdown();
+    }
+     
+    
+}
+
+
+//http://howtodoinjava.com/core-java/multi-threading/task-scheduling-with-executors-scheduledthreadpoolexecutor-example/
