c8213c00c7ab47a1981ad7fbbead3d2def9bd53c
davidblancolopez <daviddbl94@gmail.com>
2016-10-17 17:43:18 +0200

    cambios

--- src/tascaprogramada/TascaProgramada.java
+++ src/tascaprogramada/TascaProgramada.java
@@ -2,6 +2,9 @@
 package tascaprogramada;
 
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -13,6 +16,7 @@ public class TascaProgramada {
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
         ScheduledExecutorService executor = (ScheduledExecutorService) Executors.newScheduledThreadPool(2);
+        
         imprimeixExecucioTasca  iet = new imprimeixExecucioTasca ();
         
         executor.scheduleWithFixedDelay(iet, 2, 3, TimeUnit.SECONDS);
