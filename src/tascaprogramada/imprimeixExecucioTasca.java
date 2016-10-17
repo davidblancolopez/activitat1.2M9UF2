e792bce4187691a5a5c96e9d60637efe61d13ef4
davidblancolopez <daviddbl94@gmail.com>
2016-10-17 17:58:35 +0200

    cambios

--- src/tascaprogramada/imprimeixExecucioTasca.java
+++ src/tascaprogramada/imprimeixExecucioTasca.java
@@ -9,7 +9,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
 
-public class imprimeixExecucioTasca {
+public class imprimeixExecucioTasca implements Runnable {
 
     
 
